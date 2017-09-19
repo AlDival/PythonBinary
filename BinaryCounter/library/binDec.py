2508311cad95b3bb68c02fd9bc20df9b7579ba20
Cozza <32043188+darth22345@users.noreply.github.com>
2017-09-20 07:38:46 +1000

    Adding a function that makes sure the number is in binary

--- BinaryCounter/library/binDec.py
+++ BinaryCounter/library/binDec.py
@@ -1,5 +1,8 @@
 def binDec(byte):
-    while len(byte) == 8:
+    for num in byte:
+        if byte[num] in ['2','3','4','5','6','7','8','9']:
+            x = 1
+    while len(byte) == 8 and x == 0:
         decimal = 0
         for num in byte:
             decimal = decimal*2 + int(num)
@@ -7,6 +10,8 @@ def binDec(byte):
         break
     if len(byte) > 8 or len(byte) < 8:
         print("Not an 8 bit byte, please try again")
+    elif x != 0:
+        print("Please enter a valid binary number")
 
 binary = input("Byte: ")
 print(binDec(binary))

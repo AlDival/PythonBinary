aec0e7888eef7b91bb4eba232d83f6d8cc492b46
Cozza <32043188+darth22345@users.noreply.github.com>
2017-09-19 12:03:26 +1000

    Update binary-dec.py

--- BinaryCounter/binary-dec.py
+++ BinaryCounter/binary-dec.py
@@ -2,10 +2,17 @@
 n= 0
 while n < 10:
     byte = input("Byte: ")
-    decimal = 0
-    for num in byte:
-        decimal = decimal*2 + int(num)
-    print(decimal)
+    while len(byte) == 8:
+        decimal = 0
+        for num in byte:
+            decimal = decimal*2 + int(num)
+        print(decimal)
+        break
+        if byte == 'Exit':
+            print("Exiting software")
+            break
+    else:
+        print("Whoops that is not an 8 bit byte. Try again,")
     
 
 

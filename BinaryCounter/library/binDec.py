f24624026842ce9f2f8e2a59bc5d781a7de02599
ThePythonDoge <utdoge@gmail.com>
2017-09-19 09:15:17 -0600

    Update binDec.py

--- BinaryCounter/library/binDec.py
+++ BinaryCounter/library/binDec.py
@@ -4,9 +4,9 @@ def binDec(byte):
         for num in byte:
             decimal = decimal*2 + int(num)
         return(decimal)
-        break
-    if len(byte) > 8 or len(byte) < 8:
-        print("Not an 8 bit byte, please try again")
+        break 
+    if len(byte) > 8 or len(byte) < 8: #Check length of the binary number
+        print("Not an 8 bit byte, please try again.")
 
 binary = input("Byte: ")
-print(binDec(binary))
+print(binDec(binary)) #Takes "binary"'s input and passes it through the function and gives decimal result.

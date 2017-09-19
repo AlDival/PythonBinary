56131169c66bc08b76d84c09e5151f0d0faff04d
ThePythonDoge <utdoge@gmail.com>
2017-09-19 13:17:18 -0600

    Update binDec.py

--- BinaryCounter/library/binDec.py
+++ BinaryCounter/library/binDec.py
@@ -1,3 +1,19 @@
+#Binary to decimal converter
+def user_menu():
+  user_input = input('''Welcome to "binary to decimal" converter
+  [1]-Run the program
+  [2]-Exit
+  [3]-View authors\n''')
+  
+  if user_input == '1':
+    binary = input("Byte: ")
+    print(binDec(binary))
+  elif user_input == '2':
+    exit(2)
+  elif user_input == '3':
+    print("Made by: DogeSec and Darth22345.")
+    user_menu()
+
 def binDec(byte):
     while len(byte) == 8:
         decimal = 0
@@ -7,5 +23,4 @@ def binDec(byte):
     if len(byte) > 8 or len(byte) < 8: #Check length of the binary number
         print("Not an 8 bit byte, please try again.")
 
-binary = input("Byte: ")
-print(binDec(binary)) #Takes "binary"'s input and passes it through the function and gives decimal result.
+user_menu()

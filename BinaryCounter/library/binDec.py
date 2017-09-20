093cff94bdd06afddf235f857347193b51e1aa80
Cozza <32043188+darth22345@users.noreply.github.com>
2017-09-20 16:57:53 +1000

    Revert "Adding a function that makes sure the number is in binary"
    This reverts commit 2508311cad95b3bb68c02fd9bc20df9b7579ba20.

--- BinaryCounter/library/binDec.py
+++ BinaryCounter/library/binDec.py
@@ -1,8 +1,5 @@
 def binDec(byte):
-    for num in byte:
-        if byte[num] in ['2','3','4','5','6','7','8','9']:
-            x = 1
-    while len(byte) == 8 and x == 0:
+    while len(byte) == 8:
         decimal = 0
         for num in byte:
             decimal = decimal*2 + int(num)
@@ -10,8 +7,6 @@ def binDec(byte):
         break
     if len(byte) > 8 or len(byte) < 8:
         print("Not an 8 bit byte, please try again")
-    elif x != 0:
-        print("Please enter a valid binary number")
 
 binary = input("Byte: ")
 print(binDec(binary))

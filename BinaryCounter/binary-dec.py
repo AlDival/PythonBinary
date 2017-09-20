3c71fe0a9f1f40450062d85809491f3b63903c57
Cozza <32043188+darth22345@users.noreply.github.com>
2017-09-20 15:05:04 +1000

    made some further alterations such as printing out it when not in base 2

--- BinaryCounter/binary-dec.py
+++ BinaryCounter/binary-dec.py
@@ -1,22 +1,18 @@
 #Binary Converter
 import program_exit
 n= 0
-
 while n < 10:
     byte = input("Byte: ")
     byte = byte.lower()
     while len(byte) == 8:
         decimal = 0
         for num in byte:
-            if num == '1' or num == '0':
+            if num == '1' or num == '0': #only computes data in base 2
                 decimal = decimal*2 + int(num)
-            else:
-                break
         print(decimal)
-
         break
     if byte.lower() in ('exit'):
         print("Exiting software...")
         program_exit(2) #exits programs in 2 seconds and with no errors
-    elif len(byte) > 8 or len(byte) < 8:
-        print("Not an 8 bit byte, please try again")
+    elif len(byte) > 8 or len(byte) < 8 or decimal == 0:
+        print("Not an 8 bit byte. Their might also be a problem like you entered 8 0's. please try again")

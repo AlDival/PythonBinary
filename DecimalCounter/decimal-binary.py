f7758decc595f0db6127554206da90cdc349afa8
Cozza <32043188+darth22345@users.noreply.github.com>
2017-09-22 17:50:54 +1000

    Added checks to make sure number is computable

--- DecimalCounter/decimal-binary.py
+++ DecimalCounter/decimal-binary.py
@@ -1,6 +1,12 @@
 import math 
 def base2(num):
-    return 2**round((math.log2(num))) #generates the closest lower base2 number to the number provided
+    num = int(num)
+    if num > 255:
+        return("Can't fit in a byte.")
+    elif num < 0:
+        return("Negative numbers can't be computed at this time")
+    else:
+        return 2**int((math.log2(num))) #generates the closest lower base2 number to the number provided
 
-number = int(input("Number: "))#Just here for testing purposes
+number = (input("Number: "))#Just here for testing purposes
 print(base2(number))#Just here for testing purposes

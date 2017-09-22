d7a9661f8b85da572940b27b32e2e7b5a425f3dc
Cozza <32043188+darth22345@users.noreply.github.com>
2017-09-22 17:26:46 +1000

    Getting there

--- DecimalCounter/decimal-binary.py
+++ DecimalCounter/decimal-binary.py
@@ -1,1 +1,7 @@
 import math 
+def base2(num):
+    return num**int(math.log(num, 2)) #generates the closest lower base2 number to the number provided
+
+number = int(input("Number: "))
+
+print(base2(number))

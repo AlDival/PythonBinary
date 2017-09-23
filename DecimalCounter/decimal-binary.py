912f0210634c0fc989dbb2d541a5261d02a88c7e
SamuelB2020 <samuelb2020@student.stlukes.nsw.edu.au>
2017-09-23 11:24:43 +1000

    Added Conversion functionality

--- DecimalCounter/decimal-binary.py
+++ DecimalCounter/decimal-binary.py
@@ -6,6 +6,17 @@ def base2(num):
     elif num < 0:
         return("Negative numbers can't be computed at this time")
     else:
-        return 2**int((math.log2(num))) #generates the closest lower base2 number to the number provided
-
-
+        sum = num
+        list = []
+        for x in range(0,2**sum):
+            list.append(2**int((math.log2(sum))))
+            sum -= 2**int((math.log2(sum)))
+            if(sum == 0):
+                break
+        finalString = []
+        for y in range(0,8):
+            if(2**y in list):
+                finalString.append("1")
+            else:
+                finalString.append("0")
+        return "".join(finalString[::-1])
